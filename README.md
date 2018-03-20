dbdcede548007e162a8b3af56da3553f033dd830
john <nergilz@yandex.ru>
2018-03-21 01:05:20 +0200

    project added

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
-# Password Strength Calculator
+## Password Strength Calculator
 
-[TODO. There will be project description]
+ This script for password strength check
+---
 
-# Project Goals
+### description:
++ run the script without the path
++ you can specify the path to the file with a list of password
++ you can download the file here: [bad_list](https://github.com/nergilz/5_lang_frequency/password.lst) 
+
+### script start example
+```bash
+python3 password_strength.py <path to file with bad list>
+
+ input user password: <your password>
+```
+
+#### or second start example
+```bash
+python3 password_strength.py
+
+ input user password: <your password>
+```
+
+### example input
+```bash
+ strength for your password: 10
+```
+
+---
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,91 @@
+import re
+import string
+import getpass
+import argparse
 
 
-def get_password_strength(password):
-    pass
+def load_bad_list(path):
+    with open(path, 'r') as file_handler:
+        bad_list = file_handler.read()
+        return list(bad_list.split())
+
+
+def check_for_bad_list(password, dad_list):
+    if password in dad_list:
+        return -1
+    else:
+        return 1
+
+
+def check_for_upper(password):
+    if password.isupper():
+        return -1
+    else:
+        return 1
+
+
+def check_for_digit(password):
+    if password.isdigit():
+        return -3
+    else:
+        return 3
+
+
+def check_for_len(password):
+    l = len(password)
+    if l <= 5:
+        return -4
+    elif l > 5 and l <= 8:
+        return 1
+    elif l > 10:
+        return 4
+    else:
+        return 2
+
+
+def check_for_punctuation(password):
+    punct = string.punctuation
+    alpha_list = re.findall(r'.', password)
+    if punct in alpha_list:
+        return 3
+    else:
+        return 1
+
+
+def get_password_strength(password, bad_list):
+    list_strength = list()
+    list_strength.append(check_for_bad_list(password, bad_list))
+    list_strength.append(check_for_upper(password))
+    list_strength.append(check_for_digit(password))
+    list_strength.append(check_for_len(password))
+    list_strength.append(check_for_punctuation(password))
+    return sum(list_strength)
+
+
+def pprint_strength(password_strength):
+    if password_strength < 0:
+        password_strength = 1
+    print(' strength for your password: {}'.format(password_strength))
+
+
+def get_parser_args():
+    parser = argparse.ArgumentParser(description='bad list with passwords')
+    parser.add_argument(
+        'path',
+        nargs='?',
+        default='password.lst',
+        help='this is path for bad list',
+    )
+    path = parser.parse_args()
+    return path
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        argument = get_parser_args()
+        password = getpass.getpass(' input user password: ')
+        bad_list = load_bad_list(argument.path)
+        password_strength = (get_password_strength(password, bad_list))
+        pprint_strength(password_strength)
+    except FileNotFoundError:
+        print(' ERROR: file "{}" not found'.format(argument.path))
