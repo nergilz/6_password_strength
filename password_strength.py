858a6a27f69a962e117e5680b0e673cced74e08a
john <nergilz@yandex.ru>
2018-03-22 00:59:00 +0200

    all functions are updated

--- password_strength.py
+++ password_strength.py
@@ -1,71 +1,52 @@
 import re
-import string
 import getpass
 import argparse
 
 
 def load_black_list(path):
     with open(path, 'r') as file_handler:
-        black_list = list(file_handler.read().split())
+        black_list = list(file_handler.read().splitlines())
         return black_list
 
 
-def check_for_black_list(password, black_list):
-    if password in black_list:
-        return -1
-    else:
-        return 1
+def is_psw_in_black_list(password, black_list):
+    return -1 if password in black_list else 1
 
 
-def check_for_upper(password):
-    if password.isupper():
-        return -1
-    else:
-        return 1
+def has_upper(password):
+    return -1 if password.isupper() else 1
 
 
-def check_for_digit(password):
-    if password.isdigit():
-        return -3
-    else:
-        return 3
+def has_digit(password):
+    return -3 if password.isdigit() else 3
 
 
-def check_for_len(password):
+def length_check(password):
     password_length = len(password)
-    if password_length <= 5:
-        return -4
-    elif password_length > 5 and password_length <= 8:
-        return 1
-    elif password_length > 10:
-        return 4
-    else:
-        return 2
+    min_len = 5
+    middle_len = 8
+    max_len = 10
+    return -4 if password_length <= min_len \
+        else 1 if password_length > min_len and password_length <= middle_len \
+        else 3 if password_length > max_len else 2
 
 
-def check_for_punctuation(password):
-    punctuation = string.punctuation
-    alpha_list = re.findall(r'.', password)
-    if punctuation in alpha_list:
-        return 3
-    else:
-        return 1
+def is_punctuation_in_psw(password):
+    return 2 if re.search(r'\W', password) else 1
 
 
-def check_password_strength(password, black_list):
-    list_strength = list()
-    list_strength.append(check_for_black_list(password, black_list))
-    list_strength.append(check_for_upper(password))
-    list_strength.append(check_for_digit(password))
-    list_strength.append(check_for_len(password))
-    list_strength.append(check_for_punctuation(password))
-    return sum(list_strength)
+def get_password_strength(password, black_list):
+    return sum([
+        is_psw_in_black_list(password, black_list),
+        has_upper(password),
+        has_digit(password),
+        length_check(password),
+        is_punctuation_in_psw(password)
+        ])
 
 
 def pprint_strength(password_strength):
-    if password_strength < 0:
-        password_strength = 1
-    print(' strength for your password: {}'.format(password_strength))
+    print(' strength your password: {}'.format(max([password_strength, 1])))
 
 
 def get_parser_args():
@@ -76,16 +57,16 @@ def get_parser_args():
         default='password.lst',
         help='this is path for black list',
     )
-    path = parser.parse_args()
-    return path
+    arguments = parser.parse_args()
+    return arguments
 
 
 if __name__ == '__main__':
     try:
-        argument = get_parser_args()
+        arguments = get_parser_args()
         password = getpass.getpass(' input user password: ')
-        black_list = load_black_list(argument.path)
-        password_strength = (check_password_strength(password, black_list))
+        black_list = load_black_list(arguments.path)
+        password_strength = (get_password_strength(password, black_list))
         pprint_strength(password_strength)
     except FileNotFoundError:
-        print(' ERROR: file "{}" not found'.format(argument.path))
+        print(' ERROR: file "{}" not found'.format(arguments.path))
